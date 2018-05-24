b4fd04ba46e6b26d270b73917162526533643f78
madebydennis <dennis.kurochkin@ya.ru>
2018-05-24 22:50:41 +0300

    adding projects files

---
+++ projects/christmas/js/script.js
@@ -0,0 +1,13 @@
+$('.humburger').click(function () {
+	$('#nav').toggleClass('nav-active');
+	$('.humburger').toggleClass('hum-active');
+});
+//$('.jsLiElem').hover(function(){
+//    $('.jsLiElem > img').css( 'display', 'block' );
+//});
+//$('.jsLiElem').mouseout(function(){
+//    $('.jsLiElem img').css( 'display', 'none' );
+//});
+//$('.jsLiElem').hover(function(){
+//	$(this).append( '<img src="images/nav-marker.png" class="wow fadeInDownBig" alt="">' );
+//});
