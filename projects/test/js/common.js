ece0ab2960da6010b60398605600a0670a781481
madebydennis <dennis.kurochkin@ya.ru>
2018-07-13 12:50:57 +0300

    TEST

--- projects/test/js/common.js
+++ projects/test/js/common.js
@@ -172,9 +172,28 @@ $(function () {
     calculator();
 
   }
+  
+  function accord() {
+    var allPanels = $('.accordion > dd').hide();
+
+    $('.accordion > dt > a').click(function () {
+      $this = $(this);
+      $target = $this.parent().next();
+
+      if (!$target.hasClass('active')) {
+        allPanels.removeClass('active').slideUp();
+        $target.addClass('active').slideDown();
+      }
+
+      return false;
+    });
+  }
 
   if ($('body').hasClass('main') == true) {
     main();
+  }
+  if ($('body').hasClass('faq-page') == true) {
+    accord();
   }
 
 
