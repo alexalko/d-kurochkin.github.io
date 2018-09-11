4f83e4190142582ac5faf5c9d145ed96d380a251
madebydennis <dennis.kurochkin@ya.ru>
2018-09-11 20:06:32 +0300

    TEST

--- projects/test2/js/common.js
+++ projects/test2/js/common.js
@@ -1,40 +1,28 @@
 $(function() {
 
-  $('#gallery').unitegallery({
-    gallery_theme: "tiles",
-    tile_enable_textpanel: true,
-    tile_textpanel_title_text_align: "center",
-    tile_textpanel_always_on: true,
-    tiles_space_between_cols: 0,
-    tiles_space_between_cols_mobile: 0,
-    tile_textpanel_position: "inside_center",
+  // Accordeon
+  $('.toggle').click(function(e) {
+    e.preventDefault();
+
+    var $this = $(this);
+
+    if ($this.next().hasClass('show')) {
+      $this.next().removeClass('show');
+      $this.next().slideUp(350);
+      $this.removeClass('active');
+    } else {
+      $this.parent().parent().find('li .inner').removeClass('show');
+      $this.parent().parent().find('li .inner').slideUp(350);
+      $this.next().toggleClass('show');
+      $this.next().slideToggle(350);
+      $this.toggleClass('active');
+    }
   });
 
-  // 24 HRS Timer
-  setInterval(function time() {
-    var d = new Date();
-    var hours = 24 - d.getHours();
-    if ((hours + '').length == 1) {
-      hours = '0' + hours;
-    }
-    var min = 60 - d.getMinutes();
-    if ((min + '').length == 1) {
-      min = '0' + min;
-    }
-    var sec = 60 - d.getSeconds();
-    if ((sec + '').length == 1) {
-      sec = '0' + sec;
-    }
-
-    $('.jsTimerHours').html(hours);
-    $('.jsTimerMinutes').html(min);
-    $('.jsTimerSeconds').html(sec);
-
-  }, 1000);
-
-  $('.open-popup-link').magnificPopup({
-    type:'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  });
+  // Mobile menu
+	$('.nav-icon').click(function(){
+		$('.nav-icon').toggleClass('open');
+    $('.mobileMenu').toggleClass('open');
+	});
 
 });
