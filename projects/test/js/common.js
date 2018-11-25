973fb2fd6c53469579c44cf5dbc6203b8af81248
madebydennis <dennis.kurochkin@ya.ru>
2018-11-25 16:30:09 +0300

    ЕУ

--- projects/test/js/common.js
+++ projects/test/js/common.js
@@ -1,108 +1,57 @@
-$(document).ready(function () {
+$(function() {
 
-	// Функция отвечающая за полную работу ячейки "ДО ПОВЫШЕНИЯ ЦЕНЫ ТОКЕНА ОСТАЛОСЬ"
-	// Для кастомизации менять только те значения, что находятся в "changeable variables"
+	$('.toggle').click(function(e) {
+  	e.preventDefault();
+  
+    var $this = $(this);
+  
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+				$this.next().slideUp(350);
+        $this.removeClass('show');
+				
+    } else {
+        $this.parent().parent().find('li .aside_catalogueInner').removeClass('show');
+        $('li .aside_elemTitle').removeClass('show');
+        $this.parent().parent().find('li .aside_catalogueInner').slideUp(350);
+        $this.addClass('show');
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+    }
+	});
 
-			// changeable variables START
-			var monthsBefore = 9;
-			var monthsAfter = 12;
-			var newPriceContainer = $('.header_counter');
-			// changeable variables END
+	var btn = $('#toTop');
 
-			var variableYear = moment().format("YYYY");
-			var variableMonth = moment().format("MM");
-			var variableDay = moment().format("DD");
-			var nextPriceUpDay = Math.floor(moment(getNewPriceDate(variableYear + variableMonth + variableDay)) / 1000);
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 600) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
 
-			formatedDate();
-			window.setInterval(function () {
-					formatedDate();
-			}, 1000);
+	btn.on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0}, '300');
+	});
 
-			if (weeks === 0) {
-					$('.timer_week').css("display", "none");
-			}
+	$('.jsOpenHeaderPopup').click(function(e) {
+		e.preventDefault();
+		$('.callbackPopup').toggleClass('open');
+	});
 
-			function getNewPriceDay(day) {
-					var newDay = day;
-					if (parseInt(newDay) < 11) {
-							newDay = "01"
-					}
-					else if (parseInt(newDay) < 21) {
-							newDay = "11"
-					}
-					else {
-							newDay = "21"
-					}
-					return newDay;
-			}
+	$('.jsCloseHeaderPopup').click(function(e) {
+		e.preventDefault();
+		$('.callbackPopup').removeClass('open');
+	});
 
-			function formatedDate() {
-					var secondsToNextPriceUp = nextPriceUpDay - Math.floor(moment() / 1000);
-					var hoursInGreenwich = moment().tz("Europe/London").format('H');
-					var hoursHow = moment().format('H');
+	$('#callbackSecond').iziModal({
+		width: 450
+	});
 
-					amount = secondsToNextPriceUp + (hoursHow - hoursInGreenwich) * 3600;
-					if (amount > 0) {
-							weeks = Math.floor(amount / 604800);
-							amount = amount % 604800;
-							days = Math.floor(amount / 86400);
-							amount = amount % 86400;
-							hours = Math.floor(amount / 3600);
-							amount = amount % 3600;
-							mins = Math.floor(amount / 60);
-							amount = amount % 60;
-							secs = Math.floor(amount);
-
-							$('.timer_week span').text(weeks);
-							$('.timer_days span').text(days);
-							$('.timer_hours span').text(hours);
-							$('.timer_minutes span').text(mins);
-							$('.timer_seconds span').text(secs);
-					}
-			}
-
-			function getNewPriceDate(date) {
-					var newYear = date.charAt(0) + date.charAt(1) + date.charAt(2) + date.charAt(3);
-					var newMonth = date.charAt(4) + date.charAt(5);
-					var newDay = date.charAt(6) + date.charAt(7);
-					newDay = getNewPriceDay(newDay);
-
-					if (newDay === "01") {
-							newDay = "11";
-					}
-					else if (newDay === "11") {
-							newDay = "21";
-					}
-					else if (newDay === "21") {
-							newDay = "01";
-							newMonth = (parseInt(newMonth) + 1).toString()
-							if (newMonth.length === 1) {
-									newMonth = "0" + newMonth;
-							}
-					}
-					if (newMonth == "13") {
-							newMonth = "01"
-							newYear = (parseInt(newYear) + 1).toString()
-					}
-					var newDate = newYear + "-" + newMonth + "-" + newDay;
-					return newDate;
-			}
-
-		$('.jsAdvantagesSlider').owlCarousel({
-			items: 5,
-			dots: false,
-			nav: true,
-			margin: 30,
-			navText: ['<img src="../img/arrow-left-icon.png">', '<img src="../img/arrow-right-icon.png">']
-		});
-
-		$('.jsWaysSlider').owlCarousel({
-			items: 4,
-			dots: false,
-			nav: true,
-			margin: 50,
-			navText: ['<img src="../img/arrow-left-icon.png">', '<img src="../img/arrow-right-icon.png">']
-		});
+	$('.icon-one').click(function() {
+		$('.icon-one').toggleClass('active-one');
+		$('.nav_list').toggleClass('active');
+  });
 
 });
